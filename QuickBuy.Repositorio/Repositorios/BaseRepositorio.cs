8278b390ab43c6e4de25493b59301cf51054e3a2
Henrique Petrelli <henripetrelli@hotmail.com>
2020-08-10 17:17:26 -0300

    BaseRepositorio

--- QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
+++ QuickBuy.Repositorio/Repositorios/BaseRepositorio.cs
@@ -1,44 +1,46 @@
 ﻿using QuickBuy.Dominio.Contratos;
+using QuickBuy.Repositorio.Contexto;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace QuickBuy.Repositorio.Repositorios
 {
     public class BaseRepositorio<TEntiety> : IBaseRepositorio<TEntiety> where TEntiety : class
     {
-        public BaseRepositorio()
-        {
+        protected readonly QuickBuyContexto QuickBuyContexto;
 
+        //ctor Code Snippet
+        public BaseRepositorio(QuickBuyContexto quickBuyContexto)
+        {
+            QuickBuyContexto = quickBuyContexto;
         }
         public void Adicionar(TEntiety entity)
         {
-            throw new NotImplementedException();
+            QuickBuyContexto.Set<TEntiety>().Add(entity);
+            QuickBuyContexto.SaveChanges();
         }
-
         public void Atualizar(TEntiety entity)
         {
-            throw new NotImplementedException();
+            QuickBuyContexto.Set<TEntiety>().Update(entity);
+            QuickBuyContexto.SaveChanges();
         }
-
-
         public TEntiety ObterPorId(int id)
         {
-            throw new NotImplementedException();
+            return QuickBuyContexto.Set<TEntiety>().Find(id);
         }
-
         public IEnumerable<TEntiety> ObterTodos()
         {
-            throw new NotImplementedException();
+            return QuickBuyContexto.Set<TEntiety>().ToList();
         }
-
         public void Remover(TEntiety entity)
         {
-            throw new NotImplementedException();
+            QuickBuyContexto.Set<TEntiety>().Remove(entity);
+            QuickBuyContexto.SaveChanges();
         }
         public void Dispose()
         {
-            throw new NotImplementedException();
+            QuickBuyContexto.Dispose();
         }
     }
 }
